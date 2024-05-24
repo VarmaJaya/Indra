f4532237c248e1ef751d69c4b4115535b0651ad5
VarmaJaya <kiranvc12@gmail.com>
2024-05-24 00:48:38 -0700

    chanes done

--- python project.py
+++ python project.py
@@ -1,6 +1,17 @@
-num_1 = int(input("please enter your first number"))
-num_2 = int(input("please eneter your second number"))
 
-result = num_1+num_2
-
-print(result)
+print("Welcome to the HDFC Bank Registration page")
+name = str(input("please enter your name: "))
+age = int(input("please enter you age: "))
+nationality = str(input("Please enter your nationality: " ))
+acc_type = str(input("please choose your account type: 'salary', 'savings': "))
+# eligibity criteria
+if age >=18 and nationality =="indian" and acc_type == 'salary':
+    print(f"Hi {name}, You are eligible to open {acc_type} account in HDFC bank")
+    print("Please fill the below mandatory details")
+    income = int(input("please enter you anual income in lakhs"))
+    if income >= 3:
+        print("Congradulations, you can open platinum account in our bank")
+elif age >=18 and nationality =="indian" and acc_type == "savings":
+    print("Congradulations, you can open silvar account in our bank")
+else:
+    print(f"Sorry{name}, You are not eligible for to open an account in this bank")
